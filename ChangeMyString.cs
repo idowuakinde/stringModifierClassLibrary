ad66d806df218b2152d5df2bb81ae5d0a9c61328
Idowu Akinde <8018104+idowuakinde@users.noreply.github.com>
2018-12-08 17:37:50 +0100

    Example ASP.NET Core 2.1 Class Library App, based on Udemy tutorial...

---
+++ ChangeMyString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace stringModifierClassLibrary
+{
+    public class ChangeMyString
+    {
+        public static string MakeAllUpperCase(string inputString)
+        {
+            return inputString.ToUpper();
+        }
+
+        public static string MakeReverse(string inputString)
+        {
+            char[] arr = inputString.ToCharArray();
+            Array.Reverse(arr);
+            
+            return new string(arr);
+        }
+    }
+}
